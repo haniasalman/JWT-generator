07d4ba30fbf668600055c651672b3962bd415472
haniasalman <hania.salman99@gmail.com>
2023-11-14 20:18:47 +0500

    jwt generator project added

--- src/main/java/com/quadmeup/App.java
+++ src/main/java/com/quadmeup/App.java
@@ -1,28 +1,34 @@
 package com.quadmeup;
 
-import java.util.HashMap;
-import java.util.Map;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) {
         try {
-            JwtGenerator generator = new JwtGenerator();
+            String secret = "asdfSFS34wfsdfsdfSDSD32dfsddDDerQSNCK34SOWEK5354fdgdf4";
 
-            Map<String, String> claims = new HashMap<>();
-            
-            claims.put("action", "read");
-            claims.put("sub", "pawel.spychalski");
-            claims.put("email", "test@quadmeup.com");
-            claims.put("aud", "*");
+            Key hmacKey = new SecretKeySpec(Base64.getDecoder().decode(secret),
+                    SignatureAlgorithm.HS256.getJcaName());
 
-            String token = generator.generateJwt(claims);
-            System.out.println( token);
+            Instant now = Instant.now();
+            String jwtToken = Jwts.builder()
+                    .claim("name", "Aqdas")
+                    .claim("email", "aqdase@example.com")
+                    .setSubject("Aqdas JWT Demo")
+                    .setId(UUID.randomUUID().toString())
+                    .setIssuedAt(Date.from(now))
+                    .setExpiration(Date.from(now.plusSeconds(50)))
+                    .signWith(hmacKey)
+                    .compact();
+            System.out.println( jwtToken);
         } catch (Exception e) {
             e.printStackTrace();
         }
